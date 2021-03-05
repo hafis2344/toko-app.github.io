c9f7792590c0e72f20619af5c84239b51ee43b11
hafis2344 <nurhafiz20019@gmail.com>
2021-03-05 16:56:23 +0700

    tokoApp

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-array-index-key': 'off',
+    'react/prop-types': 'off',
+  },
+};
